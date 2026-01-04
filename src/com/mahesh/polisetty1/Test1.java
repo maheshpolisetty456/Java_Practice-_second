b37f34f4218ab6e4430549c1007a7c36745fd494
maheshpolisetty456 <maheshpolisetty456@gmail.com>
2026-01-04 22:30:54 +0530

    Update4 Test1.java

--- src/com/mahesh/polisetty1/Test1.java
+++ src/com/mahesh/polisetty1/Test1.java
@@ -5,8 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("mahesh is a good boy");
 		System.out.println("check-in the changes");
-		System.out.println("second time updating the project");
-		System.out.prtintln("Third time updatin the project");
+		
+		System.out.println("Third time updatin the project");
 
 	}
 
